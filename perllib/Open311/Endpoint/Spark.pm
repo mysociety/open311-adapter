mysociety/fixmystreet

Open311 Endpoint mySociety extensions role
 * Get Service Request Updates

This requires a new object ::Service::Request::Update, which of course
is not part of standard spec.  So, in order to make the core not too
contaminated by :

 * the endpoint should instantiate ::Service::Request::mySociety objects
 which know about updates
 * have added a learn_additional_types callback from Schema to Endpoint,
 so that core doesn't need to know about /open311/service_request_update
 * (but ::Spark knows about the exception for updates... meh, but is 1-line)

perllib/Open311/Endpoint/Spark.pm
@@ -102,7 +102,9 @@ sub _process_for_xml {
 
 my %singular_map = (
     service_requests => 'request',
+    service_request_updates => 'request_updates',
 );
+
 sub _singularize {
     my $name = shift;
     return $singular_map{ $name }

mysociety/open311-adapter

perllib/Open311/Endpoint/Spark.pm
@@ -1,6 +1,5 @@
 package Open311::Endpoint::Spark;
 use Moo;
-use Data::Visitor::Callback;
 
 =head1 NAME
 
@@ -102,7 +101,9 @@ sub _process_for_xml {
 
 my %singular_map = (
     service_requests => 'request',
+    service_request_updates => 'request_update',
 );
+
 sub _singularize {
     my $name = shift;
     return $singular_map{ $name }