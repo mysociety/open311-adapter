b864b7a81f46e49c5fa81628163834a7a262f6ce
Matthew Somerville <matthew-github@dracos.co.uk>
2018-03-27 13:15:35 +0100

    [Bucks] Again match commit there.

--- perllib/Integrations/Confirm/Buckinghamshire.pm
+++ perllib/Integrations/Confirm/Buckinghamshire.pm
@@ -1,24 +1,18 @@
 package Integrations::Confirm::Buckinghamshire;
 
+use Path::Tiny;
 use Moo;
 extends 'Integrations::Confirm';
 with 'Role::Config';
 
-sub endpoint_url { $_[0]->config->{CONFIRM}->{Buckinghamshire}->{url} }
-
-sub credentials {
-    my $config = $_[0]->config->{CONFIRM}->{Buckinghamshire};
-    return (
-        $config->{username},
-        $config->{password},
-        $config->{tenant_id}
-    );
-}
-
-sub server_timezone { 'Europe/London' }
-
-has '+memcache_namespace' => (
+has config_filename => (
+    is => 'ro',
     default => 'buckinghamshire_confirm',
 );
+
+sub _build_config_file {
+    my $self = shift;
+    path(__FILE__)->parent(4)->realpath->child('conf/council-' . $self->config_filename . '.yml');
+}
 
 1;
