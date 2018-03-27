7a6b9509d5addb8fad86ddcdd9760c9f4381255e
Matthew Somerville <matthew-github@dracos.co.uk>
2018-03-27 14:19:05 +0100

    [BANES] Connection config in YAML file.

--- perllib/Integrations/Confirm/BANES.pm
+++ perllib/Integrations/Confirm/BANES.pm
@@ -1,35 +1,18 @@
 package Integrations::Confirm::BANES;
 
+use Path::Tiny;
 use Moo;
 extends 'Integrations::Confirm';
 with 'Role::Config';
 
-sub endpoint_url { $_[0]->config->{CONFIRM}->{BANES}->{url} }
-
-sub credentials {
-    my $config = $_[0]->config->{CONFIRM}->{BANES};
-    return (
-        $config->{username},
-        $config->{password},
-        $config->{tenant_id}
-    );
-}
-
-has '+server_timezone' => (
-    default => 'Europe/London',
-);
-
-has '+memcache_namespace'  => (
+has config_filename => (
+    is => 'ro',
     default => 'banes_confirm',
 );
 
-has '+enquiry_method_code'  => (
-    default => 'WB' # 'Web Submission' is the EnquiryMethodName
-);
-
-has '+point_of_contact_code'  => (
-    default => 'CC' # 'Contact Centre' is the PointOfContactName
-);
-
+sub _build_config_file {
+    my $self = shift;
+    path(__FILE__)->parent(4)->realpath->child('conf/council-' . $self->config_filename . '.yml');
+}
 
 1;
