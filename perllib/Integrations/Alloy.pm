3d1ef4057914ff1be29576105eff9dec0b52b117
Dave Arter <davea@mysociety.org>
2019-01-14 17:39:46 +0000

    [Alloy] Return Open311 services based on sourceId, not sourceTypeId.
    Also returns Open311 attributes for services.
    Uses memcache to cache API calls.

--- perllib/Integrations/Alloy.pm
+++ perllib/Integrations/Alloy.pm
@@ -64,4 +64,58 @@ sub get_source_types {
     return $self->api_call("source-type", { propertyName => $self->config->{source_type_property_name} })->{sourceTypes};
 }
 
+sub get_source_for_source_type_id {
+    my $self = shift;
+    my $source_type_id = shift;
+
+    my $sources = $self->api_call("source", { sourceTypeId => $source_type_id })->{sources};
+    # TODO: Check that the source is valid - e.g. startDate/endDate/softDeleted fields
+    # TODO: What if there's zero or more than one source?
+    return @$sources[0] if @$sources;
+}
+
+sub get_parent_attribute_id {
+    my $self = shift;
+
+    return 0;
+}
+
+sub get_sources {
+    my $self = shift;
+
+    my $key = "get_sources";
+    my $expiry = 1800; # cache all these API calls for 30 minutes
+    my $sources = $self->memcache->get($key);
+    unless ($sources) {
+        $sources = [];
+        my $source_types = $self->get_source_types();
+        for my $source_type (@$source_types) {
+            my $alloy_source = $self->get_source_for_source_type_id($source_type->{sourceTypeId});
+
+            my $source = {
+                source_type_id => $source_type->{sourceTypeId},
+                description => $source_type->{description},
+                source_id => $alloy_source->{sourceId},
+                parent_attribute_id => $self->get_parent_attribute_id($source_type->{sourceTypeId}),
+            };
+
+            my @attributes = ();
+            my $source_type_attributes = $source_type->{attributes};
+            for my $source_attribute (@$source_type_attributes) {
+                push @attributes, {
+                    description => $source_attribute->{description},
+                    id => $source_attribute->{attributeId},
+                    required => $source_attribute->{required},
+                    datatype => "string", # XXX fix this
+                };
+            }
+            $source->{attributes} = \@attributes;
+
+            push @{$sources}, $source;
+        }
+        $self->memcache->set($key, $sources, $expiry);
+    }
+    return $sources;
+}
+
 1;
