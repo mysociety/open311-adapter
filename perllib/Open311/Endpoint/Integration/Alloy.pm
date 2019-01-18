e8522da19d703f70f860691b0c81bcd8b81d84f0
Dave Arter <davea@mysociety.org>
2019-01-18 08:33:19 +0000

    [Alloy] Create new Resources when receiving service requests

--- perllib/Open311/Endpoint/Integration/Alloy.pm
+++ perllib/Open311/Endpoint/Integration/Alloy.pm
@@ -1,6 +1,7 @@
 package Open311::Endpoint::Integration::Alloy;
 
 use Moo;
+use List::Util 'first';
 use DateTime::Format::W3CDTF;
 extends 'Open311::Endpoint';
 with 'Open311::Endpoint::Role::mySociety';
@@ -8,8 +9,11 @@ with 'Open311::Endpoint::Role::ConfigFile';
 
 use Open311::Endpoint::Service::UKCouncil::Alloy;
 use Open311::Endpoint::Service::Attribute;
+use Open311::Endpoint::Service::Request::CanBeNonPublic;
 
 use Path::Tiny;
+
+use Data::Dumper;
 
 
 around BUILDARGS => sub {
@@ -21,6 +25,11 @@ around BUILDARGS => sub {
 
 has jurisdiction_id => (
     is => 'ro',
+);
+
+has '+request_class' => (
+    is => 'ro',
+    default => 'Open311::Endpoint::Service::Request::CanBeNonPublic',
 );
 
 has alloy => (
@@ -54,7 +63,7 @@ sub services {
         my %service = (
             description => $source->{description},
             service_name => $source->{description},
-            service_code => $source->{source_id} . "_" . $source->{parent_attribute_id},
+            service_code => $self->service_code_for_source($source),
         );
         my $o311_service = $self->service_class->new(%service);
         for my $attrib (@{$source->{attributes}}) {
@@ -75,6 +84,74 @@ sub services {
         push @services, $o311_service;
     }
     return @services;
+}
+
+sub post_service_request {
+    my ($self, $service, $args) = @_;
+    die "No such service" unless $service;
+
+    # Get the service code from the args/whatever
+    # get the appropriate source type
+    my $sources = $self->alloy->get_sources();
+    my $source = first { $self->service_code_for_source($_) eq $service->service_code } @$sources;
+
+    printf STDERR Dumper($args);
+    printf STDERR Dumper($source);
+
+    # extract attribute values
+    my $resource_id = $args->{attributes}->{asset_resource_id} + 0;
+    my $resource = {
+        networkReference => undef,
+        sourceId => $source->{source_id},
+        parents => {
+            "$source->{parent_attribute_id}" => [ $resource_id ],
+        },
+        geoJson => {
+            type => "Point",
+            coordinates => [
+                $args->{long},
+                $args->{lat}
+            ],
+        }
+    };
+    foreach (qw/report_url fixmystreet_id northing easting asset_resource_id title description/) {
+        delete $args->{attributes}->{$_};
+    }
+    $resource->{attributes} = $args->{attributes};
+
+
+    # figure out the default values for attributes which
+    # FMS hasn't sent
+    # some will be hardcoded defaults (source etc)
+    # others computed (easting, northing, category, reported datetime etc)
+
+    # upload any photos and get their resource IDs, set attachment attribute IDs (?)
+
+    # generate the geometry
+
+    # set the parent attribute value to the resource ID
+
+    # post it up
+    my $response = $self->alloy->api_call("resource", undef, $resource);
+
+    if ($response->{errorCode}) {
+        die "Couldn't create Resource in Alloy: [$response->{errorCode} $response->{errorCodeString}] $response->{debugErrorMessage}";
+    }
+
+    # get the Alloy inspection reference
+    my $alloy_ref = $response->{resourceId};
+
+    # create a new Request and return it
+    return $self->new_request(
+        service_request_id => $alloy_ref
+    );
+
+}
+
+sub service_code_for_source {
+    my ($self, $source) = @_;
+
+    return $source->{source_id} . "_" . $source->{parent_attribute_id};
 }
 
 1;
