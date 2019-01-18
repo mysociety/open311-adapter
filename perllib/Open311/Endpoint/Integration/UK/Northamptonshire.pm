c9e97ef2218f67f48f7332782bb9810532c75c29
Dave Arter <davea@mysociety.org>
2019-01-18 14:18:42 +0000

    [Northamptonshire] Use specific inspection attribute for service request ID

--- perllib/Open311/Endpoint/Integration/UK/Northamptonshire.pm
+++ perllib/Open311/Endpoint/Integration/UK/Northamptonshire.pm
@@ -3,6 +3,8 @@ package Open311::Endpoint::Integration::UK::Northamptonshire;
 use Moo;
 extends 'Open311::Endpoint::Integration::Alloy';
 
+use List::Util 'first';
+
 around BUILDARGS => sub {
     my ($orig, $class, %args) = @_;
     $args{jurisdiction_id} = 'northamptonshire_alloy';
@@ -15,5 +17,19 @@ has integration_class => (
     is => 'ro',
     default => 'Integrations::Alloy::Northamptonshire'
 );
+
+sub service_request_id_for_resource {
+    my ($self, $resource) = @_;
+
+    my $attribute_id = $self->config->{inspection_id_attribute};
+    my $attribute = first { $_->{attributeId} eq $attribute_id } @{ $resource->{values} };
+
+    # We can't default to e.g. resourceId because it'll make later
+    # identification of this resource very complicated.
+    die "Couldn't find inspection ID for resource" unless $attribute && $attribute->{value};
+
+    return $attribute->{value};
+}
+
 
 1;
