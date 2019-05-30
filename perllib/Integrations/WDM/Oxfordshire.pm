1f00d2260a05db9c1d757388bd2ee8aa7aa421a6
Matthew Somerville <matthew-github@dracos.co.uk>
2019-05-30 14:29:49 +0100

    Mark a few statements uncoverable.

--- perllib/Integrations/WDM/Oxfordshire.pm
+++ perllib/Integrations/WDM/Oxfordshire.pm
@@ -12,6 +12,7 @@ has config_filename => (
 
 sub _build_config_file {
     my $self = shift;
+    # uncoverable statement
     path(__FILE__)->parent(4)->realpath->child('conf/council-' . $self->config_filename . '.yml');
 }
 
