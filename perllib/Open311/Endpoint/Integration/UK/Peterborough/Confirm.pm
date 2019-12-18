24b8dd93ed7cd3f0de2961c32db568f54736968a
Chris Mytton <chrismytton@gmail.com>
2019-12-18 09:37:52 +0000

    [Peterborough] Switch to Multi backend

---
+++ perllib/Open311/Endpoint/Integration/UK/Peterborough/Confirm.pm
@@ -0,0 +1,12 @@
+package Open311::Endpoint::Integration::UK::Peterborough::Confirm;
+
+use Moo;
+extends 'Open311::Endpoint::Integration::Confirm';
+
+around BUILDARGS => sub {
+    my ($orig, $class, %args) = @_;
+    $args{jurisdiction_id} = 'peterborough_confirm';
+    return $class->$orig(%args);
+};
+
+1;
