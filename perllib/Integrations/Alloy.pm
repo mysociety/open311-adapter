71336723b63c8ddcb428c5feea4445f595b08d6e
Dave Arter <davea@mysociety.org>
2020-01-20 15:19:40 +0000

    [Confirm,Alloy] Prefix Memcached namespace when running tests
    Ensures no possibility of overlap with cached data from
    open311-adapter instance.

--- perllib/Integrations/Alloy.pm
+++ perllib/Integrations/Alloy.pm
@@ -25,9 +25,11 @@ has memcache => (
     is => 'lazy',
     default => sub {
         my $self = shift;
+        my $namespace = 'open311adapter:' . $self->memcache_namespace . ':';
+        $namespace = "test:$namespace" if $ENV{TEST_MODE};
         new Cache::Memcached {
             'servers' => [ '127.0.0.1:11211' ],
-            'namespace' => 'open311adapter:' . $self->memcache_namespace . ':',
+            'namespace' => $namespace,
             'debug' => 0,
             'compress_threshold' => 10_000,
         };
