12a8da7454b6df2daa26cc48ebe789ef8b27c435
Struan Donald <struan@exo.org.uk>
2020-03-02 11:46:05 +0000

    [Hackney] remove unused category code

--- perllib/Open311/Endpoint/Integration/UK/Hackney.pm
+++ perllib/Open311/Endpoint/Integration/UK/Hackney.pm
@@ -54,14 +54,6 @@ sub process_attributes {
     # a matching contact
     my $contact_resource_id = $self->_find_or_create_contact($args);
 
-    # For category we use the group and not the category
-    my ( $group, $category ) = split('_', $args->{service_code});
-    my $group_code = $self->_find_category_code($group);
-    push @$attributes, {
-        attributeCode => $self->config->{request_to_resource_attribute_manual_mapping}->{category},
-        value => [ $group_code ],
-    };
-
     # Attach the caller to the inspection attributes
     push @$attributes, {
         attributeCode => $self->config->{contact}->{attribute_id},
