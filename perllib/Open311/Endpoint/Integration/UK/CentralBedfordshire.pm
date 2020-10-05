9e1fe6200dddb0b2234f7c972fd44a42ff983916
Dave Arter <davea@mysociety.org>
2020-10-05 18:16:06 +0100

    [Central Beds] Add mapping from area code to NextActionUsername

--- perllib/Open311/Endpoint/Integration/UK/CentralBedfordshire.pm
+++ perllib/Open311/Endpoint/Integration/UK/CentralBedfordshire.pm
@@ -13,4 +13,17 @@ has jurisdiction_id => (
 # Updates from FMS should always have a GN11 code, meaning "Customer called"
 sub event_action_event_type { 'GN11'}
 
+sub process_service_request_args {
+    my $self = shift;
+
+    my $area_code = (delete $_[0]->{attributes}->{area_code}) || '';
+    my @args = $self->SUPER::process_service_request_args(@_);
+    my $response = $args[0];
+
+    my $lookup = $self->endpoint_config->{area_to_username};
+    $response->{NextActionUserName} ||= $lookup->{$area_code};
+
+    return @args;
+}
+
 1;
