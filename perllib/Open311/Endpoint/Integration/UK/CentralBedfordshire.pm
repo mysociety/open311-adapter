643b8dd3402538ef69e0eb9a7d45bc8e4de8af16
Dave Arter <davea@mysociety.org>
2020-11-24 17:32:15 +0000

    [Central Beds] Call Symology API for rows in CSV to get complete history

--- perllib/Open311/Endpoint/Integration/UK/CentralBedfordshire.pm
+++ perllib/Open311/Endpoint/Integration/UK/CentralBedfordshire.pm
@@ -34,18 +34,108 @@ sub _get_csvs {
     return \@files;
 }
 
-sub _row_description {
-    my ($self, $row) = @_;
+sub _event_description {
+    my ($self, $event) = @_;
 
-    return $row->{"LCA Description"};
+    # return join " :: ", $event->{HistoryType}, $event->{HistoryEventType}, $event->{HistoryEventDescription}, $event->{HistoryEvent}, $event->{HistoryReference}, $event->{HistoryDescription};
+    # XXX Should this happen for all events or only certain types?
+    return $event->{HistoryDescription};
 }
 
-sub _row_status {
-    my ($self, $row) = @_;
+sub _event_status {
+    my ($self, $event) = @_;
 
-    my $stage = $row->{"Stage Desc."};
-
-    return $self->endpoint_config->{stage_mapping}->{$stage};
+    my $map = $self->endpoint_config->{event_status_mapping}->{$event->{HistoryType}};
+    return unless $map;
+    return $map->{$event->{HistoryEventType}};
 }
+
+sub get_service_request_updates {
+    my ($self, $args) = @_;
+
+    my $w3c = DateTime::Format::W3CDTF->new;
+    my $start_time = $w3c->parse_datetime($args->{start_date});
+    my $end_time = $w3c->parse_datetime($args->{end_date});
+
+    # Unlike Bexley, the CSV from the SFTP doesn't have everything we need to
+    # build the ServiceRequestUpdates. We can get the full picture from the
+    # Symology API by calling the GetRequestAdditionalGroup method for each
+    # enquiry mentioned in the CSVs and looking at the history entries there.
+
+    my %seen;
+    my $csv_files = $self->_get_csvs;
+    foreach (@$csv_files) {
+        open my $fh, '<', $_;
+
+        my $csv = Text::CSV->new;
+        $csv->header($fh, { munge_column_names => {
+            "History Date/Time" => "date_history",
+        } });
+
+        while (my $row = $csv->getline_hr($fh)) {
+            next unless $row->{CRNo} && $row->{date_history};
+            my $dt = $self->date_formatter->parse_datetime($row->{date_history});
+            next unless $dt >= $start_time && $dt <= $end_time;
+
+            $seen{$row->{CRNo}} = 1;
+        }
+    }
+
+    my @updates;
+    push(@updates, @{ $self->_updates_for_crno($_, $start_time, $end_time) }) for keys %seen;
+    return @updates;
+}
+
+sub _updates_for_crno {
+    my ($self, $crno, $start, $end) = @_;
+
+    my $response = $self->get_integration->GetRequestAdditionalGroup(
+        "SERV",
+        $crno
+    );
+
+    if (($response->{StatusCode}//-1) != 0) {
+        my $error = $response->{StatusMessage};
+        $self->log_and_die("Couldn't call GetRequestAdditionalGroup for CRNo $crno: $error");
+    }
+
+    my $history = $response->{Request}->{EventHistory}->{EventHistoryGet};
+    my @updates;
+    my $w3c = DateTime::Format::W3CDTF->new;
+    for my $event (@$history) {
+        # The event datetime is stored in two fields - both of which are datetimes
+        # but HistoryTime has today's date and HistoryDate has a midnight timestamp.
+        # So we need to reconstruct it.
+        my $date = $w3c->parse_datetime($event->{HistoryDate});
+        my $time = $w3c->parse_datetime($event->{HistoryTime});
+        $date->set(hour => $time->hour, minute => $time->minute, second => $time->second);
+        $date->set_time_zone("Europe/London");
+        next unless $date >= $start && $date <= $end;
+
+        my $update = $self->_update_for_history_event($event, $crno, $date);
+        next unless $update;
+        push @updates, $update;
+    }
+
+    return \@updates;
+}
+
+sub _update_for_history_event {
+    my ($self, $event, $crno, $dt) = @_;
+
+    my $status = $self->_event_status($event);
+    return unless $status;
+    my $description = $self->_event_description($event);
+
+    my $update_id = $crno . '_' . $event->{LineNo};
+    return Open311::Endpoint::Service::Request::Update::mySociety->new(
+        status => $status,
+        update_id => $update_id,
+        service_request_id => $crno,
+        description => $description,
+        updated_datetime => $dt,
+    );
+}
+
 
 1;
