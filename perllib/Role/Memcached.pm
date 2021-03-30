f179bdfe2349b9dfe765d6cf979182e7b836b24b
M Somerville <matthew-github@dracos.co.uk>
2021-03-30 18:05:41 +0100

    Allow config to provide a memcache namespace.

--- perllib/Role/Memcached.pm
+++ perllib/Role/Memcached.pm
@@ -3,20 +3,23 @@ package Role::Memcached;
 use Moo::Role;
 use Cache::Memcached;
 
-has memcache_namespace  => (
+has memcache_namespace => (
     is => 'lazy',
-    default => sub { $_[0]->config_filename }
+    default => sub {
+        my $namespace = $_[0]->config->{memcached_namespace} || 'open311adapter';
+        $namespace .= ':' . $_[0]->config_filename . ':';
+        return $namespace;
+    }
 );
 
 has memcache => (
     is => 'lazy',
     default => sub {
         my $self = shift;
-        my $namespace = 'open311adapter:' . $self->memcache_namespace . ':';
         my $servers = $ENV{TEST_MODE} ? [] : [ '127.0.0.1:11211' ];
         new Cache::Memcached {
             'servers' => $servers,
-            'namespace' => $namespace,
+            'namespace' => $self->memcache_namespace,
             'debug' => 0,
             'compress_threshold' => 10_000,
         };
