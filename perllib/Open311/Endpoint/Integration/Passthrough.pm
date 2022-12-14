fdbc49bd17e0bcd9c70f414d40fa830cd76ab597
Matthew Somerville <matthew@mysociety.org>
2022-12-14 14:02:48 +0000

    Add Passthrough integration.
    This will pass the data it receives through to an external Open311
    endpoint, and pass what it receives back (validating as it does).

---
+++ perllib/Open311/Endpoint/Integration/Passthrough.pm
@@ -0,0 +1,248 @@
+=head1 NAME
+
+Open311::Endpoint::Integration::Passthrough - a transparent proxy integration
+
+=head1 SUMMARY
+
+This integration passes the data on that it receives directly. This is to allow
+a third party Open311 server to sit alongside open311-adapter integrations
+within a Multi service. Validation will be performed on the data coming back.
+
+=head1 DESCRIPTION
+
+=cut
+
+package Open311::Endpoint::Integration::Passthrough;
+
+use Moo;
+extends 'Open311::Endpoint';
+with 'Open311::Endpoint::Role::mySociety';
+with 'Open311::Endpoint::Role::ConfigFile';
+with 'Role::Memcached';
+
+use DateTime::Format::W3CDTF;
+use LWP::UserAgent;
+use URI;
+use XML::Simple qw(:strict);
+use Open311::Endpoint::Service;
+use Open311::Endpoint::Service::Attribute;
+use Open311::Endpoint::Service::Request;
+use Open311::Endpoint::Service::Request::ExtendedStatus;
+use Open311::Endpoint::Service::Request::Update::mySociety;
+
+=head2 configuration
+
+To use this integration, subclass it and provide a C<jurisdiction_id> in the
+subclass's C<BUILDARGS>. This will then be used to look up a configuration file
+which should contain an C<endpoint> and an C<api_key>.
+
+=cut
+
+has jurisdiction_id => ( is => 'ro' );
+has endpoint => ( is => 'ro' );
+has api_key => ( is => 'ro' );
+
+has updates_url => ( is => 'ro', default => 'servicerequestupdates.xml' );
+
+sub service_request_content {
+    '/open311/service_request_extended'
+}
+
+# This isn't called xml because the root Endpoint class has that attribute
+has pt_xml => (
+    is => 'lazy',
+    default => sub {
+        my $group_tags = {
+            services => 'service',
+            attributes => 'attribute',
+            values => 'value',
+            service_requests => 'request',
+            errors => 'error',
+            service_request_updates => 'request_update',
+            groups => 'group',
+        };
+        XML::Simple->new(
+            SuppressEmpty => 1,
+            KeyAttr => [],
+            ForceArray => [ values %$group_tags ],
+            GroupTags => $group_tags,
+        );
+    },
+);
+
+has ua => (
+    is => 'lazy',
+    default => sub {
+        LWP::UserAgent->new();
+    },
+);
+
+has date_parser => (
+    is => 'lazy',
+    default => sub { DateTime::Format::W3CDTF->new },
+);
+
+=head2 _request
+
+Requests are made to the endpoint, and returned data parsed as XML. POST
+requests include the api_key. An error in the returned data will cause
+the function to die.
+
+=cut
+
+sub _request {
+    my ($self, $method, $url, $params) = @_;
+    $url = URI->new($self->endpoint . $url);
+    my $resp;
+    if ($method eq 'POST') {
+        $params->{api_key} = $self->api_key;
+        $resp = $self->ua->post($url, $params);
+    } else {
+        $url->query_form(%$params);
+        $resp = $self->ua->get($url);
+    }
+    my $content = $resp->decoded_content;
+    my $xml = $self->pt_xml->XMLin(\$content);
+    die $xml->{error}[0]->{description} . "\n" if $xml->{error};
+    return $xml;
+}
+
+=head2 services
+
+Transparently passed through to the backend to fetch a list of services.
+
+=cut
+
+sub services {
+    my ($self, $args) = @_;
+    my $xml = $self->_request(GET => 'services.xml');
+    my @services;
+    foreach (@{$xml->{service}}) {
+        my $service = Open311::Endpoint::Service->new(%$_);
+        if ($_->{metadata} eq 'true') {
+            # An empty one is enough to get the metadata true passed out
+            my $attribute = Open311::Endpoint::Service::Attribute->new;
+            push @{$service->attributes}, $attribute;
+        }
+        push @services, $service;
+    }
+    return @services;
+}
+
+=head2 service
+
+Transparently passed through to the backend to fetch a service.
+
+=cut
+
+sub service {
+    my ($self, $service_id, $args) = @_;
+    my $data = $self->memcache->get("service/$service_id");
+    return $data if $data;
+    my $xml = $self->_request(GET => "services/$service_id.xml");
+    my $service = Open311::Endpoint::Service->new( service_code => $service_id );
+    foreach (@{$xml->{attributes}}) {
+        $_->{required} = $_->{required} eq 'true' ? 1 : 0;
+        $_->{variable} = $_->{variable} eq 'true' ? 1 : 0;
+        # Need to maintain the order
+        $_->{values_sorted} = [ map { $_->{key} } @{$_->{values}} ];
+        $_->{values} = { map { $_->{key} => $_->{name} } @{$_->{values}} };
+        my $attribute = Open311::Endpoint::Service::Attribute->new(%$_);
+        push @{ $service->attributes }, $attribute;
+    }
+    $self->memcache->set("service/$service_id", $service, time() + 60);
+    return $service;
+}
+
+=head2 post_service_request
+
+Resets the attributes to be in the format they were received, then passes
+through to the backend to post a service request.
+
+=cut
+
+sub post_service_request {
+    my ($self, $service, $args) = @_;
+
+    _strip_args($args);
+    my $xml = $self->_request(POST => "requests.xml", $args);
+    my $id = $xml->{request}[0]{service_request_id};
+    my $result = Open311::Endpoint::Service::Request->new(service_request_id => $id);
+    return $result;
+}
+
+=head2 post_service_request_update
+
+Resets the attributes to be in the format they were received, then passes
+through to the backend to post a service request update.
+
+=cut
+
+sub post_service_request_update {
+    my ($self, $args) = @_;
+
+    _strip_args($args);
+    my $xml = $self->_request(POST => $self->updates_url, $args);
+    my $id = $xml->{request_update}[0]{update_id};
+    my $result = Open311::Endpoint::Service::Request::Update::mySociety->new(
+        update_id => $id,
+        status => lc $args->{status},
+    );
+    return $result;
+}
+
+sub _strip_args {
+    my $args = shift;
+    # Yes, just reversing what has just been done
+    foreach my $k (keys %{$args->{attributes}}) {
+        $args->{"attribute[$k]"} = $args->{attributes}{$k};
+    }
+    delete $args->{attributes};
+}
+
+=head2 get_service_request_updates
+
+Fetches a list of updates from the backend.
+
+=cut
+
+sub get_service_request_updates {
+    my ($self, $args) = @_;
+    my $xml = $self->_request(GET => $self->updates_url, $args);
+    my @updates;
+    foreach (@{$xml->{request_update}}) {
+        $_->{status} = lc $_->{status};
+        $_->{updated_datetime} = $self->date_parser->parse_datetime($_->{updated_datetime});
+        $_->{description} ||= "";
+        my $update = Open311::Endpoint::Service::Request::Update::mySociety->new(%$_);
+        push @updates, $update;
+    }
+    return @updates;
+}
+
+=head2 get_service_requests
+
+Fetches a list of requests from the backend.
+
+=cut
+
+sub get_service_requests {
+    my ($self, $args) = @_;
+    my $xml = $self->_request(GET => "requests.xml", $args);
+    my @requests;
+    foreach (@{$xml->{request}}) {
+        $_->{status} = lc $_->{status};
+        $_->{updated_datetime} = $self->date_parser->parse_datetime($_->{updated_datetime});
+        $_->{requested_datetime} = $self->date_parser->parse_datetime($_->{requested_datetime});
+        $_->{latlong} = [ delete $_->{lat}, delete $_->{long} ];
+        $_->{service} = Open311::Endpoint::Service->new(
+            service_name => delete $_->{service_name},
+            service_code => delete $_->{service_code},
+        );
+        my $request = Open311::Endpoint::Service::Request::ExtendedStatus->new(%$_);
+        push @requests, $request;
+    }
+    return @requests;
+}
+
+1;
--- t/open311/endpoint/bromley.t
+++ t/open311/endpoint/bromley.t
@@ -10,6 +10,16 @@ around BUILDARGS => sub {
     return $class->$orig(%args);
 };
 
+package Open311::Endpoint::Integration::UK::Bromley::Passthrough;
+use Moo;
+extends 'Open311::Endpoint::Integration::Passthrough';
+around BUILDARGS => sub {
+    my ($orig, $class, %args) = @_;
+    $args{jurisdiction_id} = 'passthrough_dummy';
+    $args{config_data} = "endpoint: URL/\napi_key: 123";
+    return $class->$orig(%args);
+};
+
 package main;
 
 use strict;
@@ -63,6 +73,39 @@ subtest "Get service definition with no prefix" => sub {
             }
           ]
         }, 'correct json returned';
+};
+
+my $expected_update_post = <<XML;
+<?xml version="1.0" encoding="utf-8"?>
+<service_request_updates>
+  <request_update>
+    <update_id>2002</update_id>
+  </request_update>
+</service_request_updates>
+XML
+my $lwp = Test::MockModule->new('LWP::UserAgent');
+$lwp->mock(request => sub {
+    my ($ua, $req) = @_;
+    return HTTP::Response->new(200, 'OK', [], $expected_update_post);
+});
+
+subtest 'POST update' => sub {
+    my $res = $endpoint->run_test_request(
+        POST => '/servicerequestupdates.xml',
+        jurisdiction_id => 'bromley',
+        service_code => 'WINTER_SNOW',
+        api_key => 'test',
+        service_request_id => 1001,
+        update_id_ext => 123,
+        first_name => 'Bob',
+        last_name => 'Mould',
+        description => 'Update here',
+        status => 'OPEN',
+        updated_datetime => '2016-09-01T15:00:00Z',
+        media_url => 'http://example.org/',
+    );
+    ok $res->is_success, 'valid request' or diag $res->content;
+    is $res->content, $expected_update_post, 'xml string ok' or diag $res->content;
 };
 
 done_testing;
