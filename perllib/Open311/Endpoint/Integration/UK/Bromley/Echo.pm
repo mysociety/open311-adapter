7f0073113bc218fe43e5d9f541ca4b2a3f5a88b3
Matthew Somerville <matthew@mysociety.org>
2024-03-22 15:05:06 +0000

    [Bromley] Include Title, and single-line Notes.

--- perllib/Open311/Endpoint/Integration/UK/Bromley/Echo.pm
+++ perllib/Open311/Endpoint/Integration/UK/Bromley/Echo.pm
@@ -30,9 +30,29 @@ start/end/review dates; when removing, we set the action and end date.
 
 =cut
 
+sub echo_title_id {
+    my $title = shift;
+    return 1 if $title eq 'MR';
+    return 2 if $title eq 'MRS';
+    return 3 if $title eq 'MISS';
+    return 4 if $title eq 'MS';
+    return 5 if $title eq 'DR';
+    return 7;
+}
+
 around process_service_request_args => sub {
     my ($orig, $class, $args) = @_;
     my $request = $class->$orig($args);
+
+    if (!$args->{attributes}{Notes}) {
+        $args->{attributes}{Notes} = join(" | ", $args->{attributes}{report_title} || (), $args->{description} || ());
+    }
+    $args->{attributes}{Notes} =~ s/(\r?\n)+/ | /g;
+
+    if (my $title = $args->{attributes}{title}) {
+        $args->{attributes}{Title} = echo_title_id($title);
+    }
+
     # Assisted collection
     if ($request->{event_type} == 2149) {
         my $date = DateTime->today(time_zone => "Europe/London");
