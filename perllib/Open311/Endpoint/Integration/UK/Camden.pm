6e6bfcc58119b88a1b7f00be9578ce3c8e7d4e13
Moray Jones <moray@mysociety.org>
2024-06-13 09:14:22 +0100

    [Camden] Adds integration without prefix
    Allow the original Symology integration to continue
    without a prefix on its categories so any reports
    and updates will still match as before being
    made a multi

--- perllib/Open311/Endpoint/Integration/UK/Camden.pm
+++ perllib/Open311/Endpoint/Integration/UK/Camden.pm
@@ -12,4 +12,17 @@ has jurisdiction_id => (
     default => 'camden',
 );
 
+=pod
+
+Camden was previously only a Symology backend in open311-adapter, so we
+maintain its categories/IDs without any backend prefix as any updates on pre-multi
+reports will be looking for the id without the prefix
+
+=cut
+
+has integration_without_prefix => (
+    is => 'ro',
+    default => 'Symology',
+);
+
 __PACKAGE__->run_if_script;
