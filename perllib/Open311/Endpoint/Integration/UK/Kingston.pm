b62cf81731cebb3373acd61d8f692b5b7e18f474
Matthew Somerville <matthew@mysociety.org>
2024-06-14 14:19:29 +0100

    [Kingston] Spot payment method in garden too.

--- perllib/Open311/Endpoint/Integration/UK/Kingston.pm
+++ perllib/Open311/Endpoint/Integration/UK/Kingston.pm
@@ -10,4 +10,23 @@ around BUILDARGS => sub {
     return $class->$orig(%args);
 };
 
+around check_for_data_value => sub {
+    my ($orig, $class, $name, $args, $request, $parent_name) = @_;
+
+    # Garden waste
+    if ($args->{service_code} eq '1638') {
+        my $method = $args->{attributes}{payment_method} || '';
+        if ($name eq 'Payment Type') {
+            return 3 if $method eq 'csc' || $method eq 'cheque'; # Telephone
+        }
+        if ($name eq 'Payment Method') {
+            return 1 if $method eq 'csc' || $method eq 'cheque'; # Borough Phone Payment
+            return 2 if $method eq 'credit_card'; # Borough Website Payment
+        }
+    }
+
+
+    return $class->$orig($name, $args, $request, $parent_name);
+};
+
 1;
