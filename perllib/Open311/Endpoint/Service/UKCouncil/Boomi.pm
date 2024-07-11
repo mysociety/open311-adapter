8d4efbfbeb381fef058fed2da45194f6d5a8eca7
Dave Arter <davea@mysociety.org>
2024-07-11 22:43:39 +0100

    [Surrey] Allow multivaluelist attributes through

--- perllib/Open311/Endpoint/Service/UKCouncil/Boomi.pm
+++ perllib/Open311/Endpoint/Service/UKCouncil/Boomi.pm
@@ -44,6 +44,49 @@ sub _build_attributes {
             required => 1,
             automated => 'server_set',
         ),
+
+        # XXX these should not be hardcoded here but haven't yet figured out
+        # how to allow any attribute/value to pass schema validation
+        Open311::Endpoint::Service::Attribute->new(
+            code => "Q7",
+            description => "Q7",
+            datatype => "multivaluelist",
+            required => 0,
+            variable => 1,
+            allow_any_value => 1,
+        ),
+        Open311::Endpoint::Service::Attribute->new(
+            code => "pothole_severity",
+            description => "pothole_severity",
+            datatype => "multivaluelist",
+            required => 0,
+            variable => 1,
+            allow_any_value => 1,
+        ),
+        Open311::Endpoint::Service::Attribute->new(
+            code => "pothole_location",
+            description => "pothole_location",
+            datatype => "multivaluelist",
+            required => 0,
+            variable => 1,
+            allow_any_value => 1,
+        ),
+        Open311::Endpoint::Service::Attribute->new(
+            code => "D1_Declaration",
+            description => "D1_Declaration",
+            datatype => "multivaluelist",
+            required => 0,
+            variable => 1,
+            allow_any_value => 1,
+        ),
+        Open311::Endpoint::Service::Attribute->new(
+            code => "1_Location",
+            description => "1_Location",
+            datatype => "multivaluelist",
+            required => 0,
+            variable => 1,
+            allow_any_value => 1,
+        ),
     );
 
     return \@attributes;
