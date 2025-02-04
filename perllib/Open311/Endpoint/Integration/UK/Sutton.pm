a2e1611a808606c9b5f92a452ca49ee4a3c6d7fa
Matthew Somerville <matthew@mysociety.org>
2025-02-04 15:44:30 +0000

    [Sutton] Update for new Echo details.

--- perllib/Open311/Endpoint/Integration/UK/Sutton.pm
+++ perllib/Open311/Endpoint/Integration/UK/Sutton.pm
@@ -4,9 +4,7 @@ Open311::Endpoint::Integration::UK::Sutton
 
 =head1 DESCRIPTION
 
-The Sutton integration. As well as the boilerplate, and setting it as an Echo
-integration, this makes sure the right "Payment Taken By" option is chosen for
-bulky collections.
+The Sutton integration. Boilerplate, and setting it as an Echo integration.
 
 =cut
 
@@ -20,28 +18,6 @@ around BUILDARGS => sub {
     my ($orig, $class, %args) = @_;
     $args{jurisdiction_id} = 'sutton_echo';
     return $class->$orig(%args);
-};
-
-around check_for_data_value => sub {
-    my ($orig, $class, $name, $args, $request, $parent_name) = @_;
-
-    # Bulky items
-    if ($args->{service_code} eq '1636') {
-        my $method = $args->{attributes}{payment_method} || '';
-        if ($method eq 'csc' || $method eq 'cheque') {
-            return 1 if $name eq 'Payment Taken By'; # Council
-        } elsif ($method eq 'credit_card') {
-            return 2 if $name eq 'Payment Taken By'; # Veolia
-        }
-    }
-
-    # Garden waste
-    if ($args->{service_code} eq '1638') {
-        my $method = $args->{attributes}{payment_method} || '';
-        return 3 if $name eq 'Payment Type' && $method eq 'cheque'; # Telephone
-    }
-
-    return $class->$orig($name, $args, $request, $parent_name);
 };
 
 has cancel_actiontype_id => ( is => 'ro', default => 8 );
