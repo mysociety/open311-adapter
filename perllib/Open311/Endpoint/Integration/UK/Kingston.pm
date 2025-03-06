ef6e80c642571c4e112c7cd4451214d2a31b45fb
Matthew Somerville <matthew@mysociety.org>
2025-03-06 22:26:15 +0000

    [Kingston] New cancel action ID.

--- perllib/Open311/Endpoint/Integration/UK/Kingston.pm
+++ perllib/Open311/Endpoint/Integration/UK/Kingston.pm
@@ -10,6 +10,6 @@ around BUILDARGS => sub {
     return $class->$orig(%args);
 };
 
-has cancel_actiontype_id => ( is => 'ro', default => 8 );
+has cancel_actiontype_id => ( is => 'ro', default => 518 );
 
 1;
