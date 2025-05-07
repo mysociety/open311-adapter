e74c8c04d310c22dfc4c1b27a8de123afc4e5bb1
Dave Arter <davea@mysociety.org>
2025-05-07 12:25:12 +0100

    [Confirm] Allow fetching of Enquiries that have duped service codes

--- perllib/Open311/Endpoint/Integration/Confirm.pm
+++ perllib/Open311/Endpoint/Integration/Confirm.pm
@@ -710,8 +710,7 @@ sub _services {
     for my $group (sort keys %$service_whitelist) {
         my $whitelist = $fetch_all_services ? \%services : $self->service_whitelist->{$group};
         for my $code (keys %{ $whitelist }) {
-            my ($serv, $subj, $extra) = split /_/, $code;
-            my $confirm_code = join("_", $serv, $subj);
+            my $confirm_code = _normalise_service_code($code);
             my $subject = $services{$confirm_code}->{subject};
             if (!$subject) {
                 $self->logger->error("$confirm_code doesn't exist in Confirm.");
@@ -733,8 +732,7 @@ sub _services {
         }
     }
     for my $code (sort keys %service_codes) {
-        my ($serv, $subj, $extra) = split /_/, $code;
-        my $confirm_code = join("_", $serv, $subj);
+        my $confirm_code = _normalise_service_code($code);
         my %service = %{ $service_codes{$code} };
         my $o311_service = $self->service_class->new(%service);
         for (@{$services{$confirm_code}->{attribs}}) {
@@ -806,9 +804,16 @@ sub get_service_requests {
     my $integ = $self->get_integration;
     my @requests;
     my @services = $self->services;
+    # Some Confirm configurations map multiple Open311 services to one Confirm
+    # service/subject code - these are indicated with a _1/_2/etc suffix in
+    # service_whitelist. When fetching Enquiries from Confirm we join the
+    # service & subject code with _ and lookup the corresponding Open311
+    # service, which might fail if only the _1 suffixed version exists. So here
+    # we build a list of services to match against by stripping any _1/etc
+    # suffixes. As a result the first matching service will be used.
     my %services = map {
-        $_->{service_code} => $_
-    } @services;
+        _normalise_service_code($_->{service_code}) => $_
+    } reverse @services;
     my %private_services = map { $_ => 1 } @{$self->private_services};
 
     # Enquiries
@@ -1114,5 +1119,10 @@ sub get_completion_photo {
     return [ 200, [ 'Content-type', $content_type ], [ $content ] ];
 }
 
+sub _normalise_service_code {
+    my $code = shift;
+    my ($serv, $subj, $extra) = split /_/, $code;
+    return join("_", $serv, $subj);
+}
 
 1;
