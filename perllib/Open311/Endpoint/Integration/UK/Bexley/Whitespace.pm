ab117a5a4fc3505a66e008369f9bcef5e6d7c150
Victoria Mihell-Hale <victoria.mihellhale@hotmail.co.uk>
2025-08-05 16:31:57 +0100

    [Bexley][Bulky] Custom worksheet message
    No longer includes pension or disability info.

--- perllib/Open311/Endpoint/Integration/UK/Bexley/Whitespace.pm
+++ perllib/Open311/Endpoint/Integration/UK/Bexley/Whitespace.pm
@@ -12,16 +12,42 @@ around BUILDARGS => sub {
 sub _worksheet_message {
     my ($self, $args) = @_;
 
-    my @messages;
-    foreach (
+    my @attributes = $args->{service_code} eq 'bulky_collection' ? (
+        { key => 'fixmystreet_id', label => 'Booking reference:' },
+        { key => 'collection_date', label => 'Collection date:' },
+        { key => 'bulky_location', label => 'Location of items:' },
+        { key => 'bulky_parking', label => 'Parking restrictions:' },
+    ) : (
         { key => 'assisted_yn', label => 'Assisted collection?' },
         { key => 'location_of_containers', label => 'Location of containers:' },
         { key => 'location_of_letterbox', label => 'Location of letterbox:' },
-        { key => 'pension', label => 'State pension?' },
-        { key => 'disability', label => 'Physical disability?' },
-    ) {
-        push @messages, "$_->{label} $args->{attributes}->{$_->{key}}"
-            if $args->{attributes}->{$_->{key}};
+    );
+
+    my @messages;
+    foreach (@attributes) {
+        my $val = $args->{attributes}->{ $_->{key} };
+
+        next unless $val;
+
+        if ( $_->{key} eq 'collection_date' ) {
+            my $orig_val = $val;
+
+            my $dtf_from = DateTime::Format::Strptime->new(
+                pattern => '%Y-%m-%d',
+            );
+            my $dtf_to = DateTime::Format::Strptime->new(
+                pattern => '%d/%m/%Y',
+            );
+
+            # Undef if error
+            $val = $dtf_from->parse_datetime($val);
+            $val = $dtf_to->format_datetime($val) if $val;
+
+            # Just send original string if it's faulty
+            $val = $orig_val unless $val;
+        }
+
+        push @messages, "$_->{label} $val";
     }
 
     return join("\n\n", @messages);
