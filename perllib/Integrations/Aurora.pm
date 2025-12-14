223dcae13b3e65a5f3d796c8dd5b2e90cfd3e695
Nik Gupta <vngupta77@gmail.com>
2025-12-14 00:04:05 +0000

    [Aurora] Add contact get and create methods.

--- perllib/Integrations/Aurora.pm
+++ perllib/Integrations/Aurora.pm
@@ -15,11 +15,13 @@ package Integrations::Aurora;
 use strict;
 use warnings;
 
+use HTTP::Request;
 use HTTP::Request::Common;
 use JSON::MaybeXS;
 use LWP::UserAgent;
 use Moo;
 use MIME::Base64 qw(decode_base64);
+use URI::Escape;
 
 with 'Role::Config';
 with 'Role::Logger';
@@ -68,6 +70,11 @@ has oauth_access_token_url => (
     default => sub { $_[0]->config->{oauth}->{access_token_url} }
 );
 
+has cases_api_base_url => (
+    is => 'lazy',
+    default => sub { $_[0]->config->{cases_api_base_url} }
+);
+
 has ua => (
     is => 'lazy',
     default => sub {
@@ -102,5 +109,90 @@ has access_token => (
         return $token;
     },
 );
+
+=head1 METHODS
+
+=cut
+
+=head2 get_contact_id_for_email_address
+
+Returns, the ID of the first contact found with a matching email,
+or undef if no match is found.
+Assumes any match will always be in the first page of results.
+
+=cut
+
+sub get_contact_id_for_email_address {
+    my ($self, $email_address) = @_;
+    my $token = $self->access_token or die "Failed to get access token.";
+    my $request = GET $self->cases_api_base_url .
+        "Cases/Contact?emailAddress=" . uri_escape($email_address),
+        Authorization => "Bearer $token",
+    ;
+    my $response = $self->ua->request($request);
+    if (!$response->is_success) {
+        $self->_fail("Failed to query contacts", $request, $response);
+    }
+    my $content = decode_json($response->content);
+    foreach (@{$content->{contacts}}) {
+        if ($_->{emailAddress} eq $email_address) {
+            return $_->{id};
+        }
+    };
+    return undef;
+}
+
+=head2 create_contact_and_get_id
+
+Creates a contact with the given email, first name, last name and
+number, and returns its ID.
+
+=cut
+
+sub create_contact_and_get_id {
+    my ($self, $email_address, $first_name, $last_name, $number) = @_;
+    my $token = $self->access_token or die "Failed to get access token.";
+    my $payload = {
+        firstName => $first_name,
+        lastName => $last_name,
+        emailAddress => $email_address,
+    };
+    if ($number) {
+        $number =~ s/\D//g;  # remove non-digits
+        $number = "0$number" if $number =~ /^44/;  # replace country code with 0
+        if ($number =~ /^07/) {
+            $payload->{mobilePhone} = $number;
+        } else {
+            $payload->{homePhone} = $number;
+        }
+    }
+    my $request = HTTP::Request->new(
+        'POST',
+        $self->cases_api_base_url . "Cases/Contact/CreateContact",
+        [
+            Authorization => "Bearer $token",
+            "Content-Type" => "application/json",
+        ],
+        encode_json($payload)
+    );
+    my $response = $self->ua->request($request);
+    if (!$response->is_success) {
+        $self->_fail("Failed to create contact", $request, $response);
+    }
+    my $content = decode_json($response->content);
+    return $content->{contactId};
+}
+
+
+sub _fail {
+    my ($self, $message, $request, $response) = @_;
+    my $request_string = $request->as_string;
+    $request_string =~ s/(Authorization: ).+/$1\[REDACTED\]/;
+    $self->logger->error(sprintf(
+        "%s\n\nRequested:\n\n%s\n\nGot:\n\n%s",
+        $message, $request_string, $response->as_string
+    ));
+    die $message;
+}
 
 1;
