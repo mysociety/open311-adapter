c34cc64707ff236cfa6ae95dd0c84283556a4471
Matthew Somerville <matthew@mysociety.org>
2025-12-17 15:57:08 +0000

    [Enfield] A couple of forms need specific fields.
    One form needs a date noticed field and one needs a specific value.

--- perllib/Open311/Endpoint/Integration/Verint.pm
+++ perllib/Open311/Endpoint/Integration/Verint.pm
@@ -59,6 +59,12 @@ sub post_service_request {
             . "\n\nProblem details: " . $args->{attributes}->{description}
             . "\n\nLink: " . $args->{attributes}->{report_url};
     }
+    if ($service_cfg->{form_name} eq 'lbe_saftey_barrier_new') {
+        $extra{dt_date_noticed_problem} = $date->date;
+    }
+    if ($service_cfg->{form_name} eq 'lbe_illuminated_bollard') {
+        $extra{rad_which_sign_problem} = 'Report an unlit illuminated sign';
+    }
 
     my $result = $integ->CreateRequest(
         $service_cfg->{form_name},
